15dfb31187c8186c2db5811f7a346076986eaca3
Prueba1 <jonapardo6@gmail.com>
2024-11-20 09:00:31 -0500

    CABECERITOs

--- src/main/java/controlador/ProductoXls.java
+++ src/main/java/controlador/ProductoXls.java
@@ -27,43 +27,51 @@ public class ProductoXls extends HttpServlet {
         String servletPath = req.getServletPath();
         //CREO UNA VARIBLE DE TIPO BOLEANO PARA VERIFICAR QUE PATH
         //ESTOY OBTENIENDO
-        boolean xls = servletPath.equals(".xls");
-        if(xls){
+        boolean xls = servletPath.endsWith(".xls");
+        if (xls) {
             resp.setContentType("application/vnd.ms-excel");
             resp.setHeader("Content-disposition", "attachment; filename=productos.xls");
         }
         resp.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = resp.getWriter();
+        try (PrintWriter out = resp.getWriter()) {
+            if (!xls) {
 
-        out.println("<!DOCTYPE html>");
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<meta charset='UTF-8'>");
-        out.println("<title>Lista de productos</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<h1>Listado de productos</h1>");
-        out.println("<p><a href=\"" + req.getContextPath() + "/producto.xls"+"\">Descargar xls" + "</a></p>");
-        out.println("<table>");
-        out.println("<tr>");
-        out.println("<th>ID PRODUCTO</th>");
-        out.println("<th>NOMBRE</th>");
-        out.println("<th>CATEGORIA</th>");
-        out.println("<th>PRECIO</th>");
-        out.println("</tr>");
-        productos.forEach(p -> {
+
+                out.println("<!DOCTYPE html>");
+                out.println("<html>");
+                out.println("<head>");
+                out.println("<meta charset='UTF-8'>");
+                out.println("<title>Lista de productos</title>");
+                out.println("</head>");
+                out.println("<body>");
+                out.println("<h1>Listado de productos</h1>");
+                out.println("<p><a href=\"" + req.getContextPath() + "/producto.xls" + "\">Descargar xls" + "</a></p>");
+            }
+                out.println("<table>");
             out.println("<tr>");
-            out.println("<td>" + p.getIdProducto() + "</td>");
-            out.println("<td>" + p.getNombre() + "</td>");
-            out.println("<td>" + p.getCategoria() + "</td>");
-            out.println("<td>" + p.getPrecio() + "</td>");
+            out.println("<th>ID PRODUCTO</th>");
+            out.println("<th>NOMBRE</th>");
+            out.println("<th>CATEGORIA</th>");
+            out.println("<th>PRECIO</th>");
             out.println("</tr>");
-        });
-        out.println("</table>");
-        out.println("</body>");
-        out.println("</html>");
-    }
+            productos.forEach(p -> {
+                out.println("<tr>");
+                out.println("<td>" + p.getIdProducto() + "</td>");
+                out.println("<td>" + p.getNombre() + "</td>");
+                out.println("<td>" + p.getCategoria() + "</td>");
+                out.println("<td>" + p.getPrecio() + "</td>");
+                out.println("</tr>");
+            });
+            out.println("</table>");
 
+            if (!xls) {
+
+                out.println("</body>");
+                out.println("</html>");
+            }
+        }
+
+    }
 }
 
 
